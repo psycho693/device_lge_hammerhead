f43768c107126eae78fd890155278e51cad6f5d3
sykopompos <sykopompos@gmail.com>
2014-11-11 03:25:02 -0500

    move build.prop overrides here

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -33,7 +33,31 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_BUILD_PROP_OVERRIDES +=
 BUILD_FINGERPRINT :=google/hammerhead/hammerhead:5.0/LRX21M/1511060136:user/release-keys 
 PRIVATE_BUILD_DESC :="hammerhead-userdebug 5.0 LRX21M 11060136 release-keys"
-BUILD_DISPLAY=LRX21M-FrankenBunny
+BUILD_DISPLAY_ID=LRX21M-FrankenBunny
+# Common build prop overrides 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Titania.ogg \
+    ro.config.notification_sound=Tethys.ogg \
+    ro.config.alarm_alert=Oxygen.ogg \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.ril.enable.amr.wideband=1 \
+    net.tethering.noprovisioning=true \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    keyguard.no_require_sim=true \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.setupwizard.network_required=false \
+    ro.setupwizard.gservices_delay=-1
 
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
